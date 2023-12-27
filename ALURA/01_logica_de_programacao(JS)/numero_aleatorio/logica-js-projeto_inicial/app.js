ade772c37274e7437f0a11ab3d4d634c9e279898
leocaastro <143289383+leocaastro@users.noreply.github.com>
2023-12-27 10:17:36 -0300

    Add files via upload

---
+++ ALURA/01_logica_de_programacao(JS)/numero_aleatorio/logica-js-projeto_inicial/app.js
@@ -0,0 +1,13 @@
+alert('Bem vindo ao Jogo do Número secreto');
+let numeroSecreto = 29;
+
+let senhaDoSistema = "senhaTeste!";
+
+let chute = prompt('Escolha um número entre 1 e 30:');
+ 
+alert(senhaDoSistema);
+
+if (chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto (29)');
+}
+
